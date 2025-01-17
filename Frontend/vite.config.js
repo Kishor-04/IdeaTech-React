167f93ef80aef4f3e2a70969d103bd61838c161d
Kishor-04 <kishorpatil.connect@gmail.com>
2025-01-18 05:26:18 +0530

    Adding path of directory in backend to configure for hosting

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // Ensure the app knows the correct base path
   plugins: [react()],
   build: {
     outDir: 'dist', // or your specific output directory
