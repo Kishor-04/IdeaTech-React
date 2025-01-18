c5b14cc0f76495517e2be578ba8081f5e6ea9535
Kishor-04 <kishorpatil.connect@gmail.com>
2025-01-18 05:53:25 +0530

    Changes made in vite.config.js for hosting

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // or your specific output directory
-  },
+  server:{
+    "/": {
+      target:"https://ideatech-react.onrender.com",
+      changeOrigin: true
+    }
+  }
 })
+// This is the configuration file for Vite. It is a build tool that is used to bundle our React application.
